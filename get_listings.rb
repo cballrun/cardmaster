2daaf2733f0c37b2473a825f34f02c9107d6b449
cballrun <carter.k.ball@gmail.com>
2024-03-07 10:47:56 -0500

    GetListings setup

---
+++ get_listings.rb
@@ -0,0 +1,10 @@
+require "selenium-webdriver"
+require "interactor"
+
+def GetListings
+include Interactor
+    
+def call
+
+    end
+end
--- scraping_organizer.rb
+++ scraping_organizer.rb
@@ -1,11 +1,12 @@
 require "selenium-webdriver"
 require "interactor"
 require_relative "get_latest_sales"
+require_relative "get_listings"
 
 
 
 class ScrapingOrganizer
     include Interactor::Organizer
 
-    organize GetLatestSales
+    organize GetLatestSales, GetListings
 end
