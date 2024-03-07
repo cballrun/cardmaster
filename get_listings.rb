1432d29431875da52a0978cf8dba29b261f0ff70
cballrun <carter.k.ball@gmail.com>
2024-03-07 14:07:33 -0500

    GetListings able to navigate to all pages of listings and output condition, price, and seller to CSV

--- get_listings.rb
+++ get_listings.rb
@@ -5,10 +5,14 @@ class GetListings
 include Interactor
     CardListing = Struct.new(:condition, :price, :seller, keyword_init: true)
 
-
     def call
-        sleep 2
-        create_listings(context.driver, context.wait)
+        CSV.open("listings.csv", "wb", write_headers: true, headers: ["condition", "price", "seller"]) do |csv|
+            while next_page_button_visible?(context.wait, context.driver) do
+                create_listings(context.driver, context.wait, csv)
+                next_page_link(context.driver)
+                next_page_button(context.driver)
+            end
+        end
     end
 
     private 
@@ -19,8 +23,7 @@ include Interactor
         end
     end
 
-    def create_listings(driver, wait)
-        all_listings = []
+    def create_listings(driver, wait, csv)
         listings = find_listings(driver, wait)
         listings.each do |listing|
             condition = listing.find_element(:css, "h3").text
@@ -28,8 +31,26 @@ include Interactor
             seller = listing.find_element(:css, "a.seller-info__name").text
 
             card_listing = CardListing.new(condition: condition, price: price, seller: seller)
-            all_listings << card_listing
+            csv << card_listing
         end
-        all_listings
+    end
+
+    def next_page_link(driver)
+        pagination_buttons = driver.find_elements(:css, "a.tcg-button.tcg-button--md.tcg-standard-button.tcg-standard-button--flat")
+        next_page_link = pagination_buttons.last.attribute("href")
+        if next_page_link.class == String
+            driver.get next_page_link
+        else
+            driver.quit
+        end
+    end
+
+    def next_page_button(driver)
+        next_button = driver.find_elements(:css, "a.tcg-button.tcg-button--md.tcg-standard-button.tcg-standard-button--flat").last
+    end
+
+    def next_page_button_visible?(wait, driver)
+        sleep 2
+        wait.until { next_page_button(driver).displayed? }
     end
 end
